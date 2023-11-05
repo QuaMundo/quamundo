e2faf560f3a537a85ad7eb8eda6f4e59b790ab7c
Alexander Trieschmann <alexander.trieschmann@posteo.de>
2023-11-05 01:17:32 +0100

    configure rails for testing with cypress
    prepare hooks for cypress db test data
    
    Configure preparation and cleanup of test data for cypress testing.

---
+++ config/initializers/cypress_rails.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+return unless Rails.env.test?
+
+Rails.application.load_tasks unless defined?(Rake::Task)
+
+CypressRails.hooks.before_server_start do
+  # Called once, before either the transaction or the server is started
+  Rake::Task['db:seed'].invoke
+end
+
+CypressRails.hooks.after_transaction_start do
+  # Called after the transaction is started (at launch and after each reset)
+end
+
+CypressRails.hooks.after_state_reset do
+  # Triggered after `/cypress_rails_reset_state` is called
+end
+
+CypressRails.hooks.before_server_stop do
+  # Called once, at_exit
+  Rake::Task['db:test:prepare'].invoke
+end
