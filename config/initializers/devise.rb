f10a8ee25d87ebe31bc185b3a2c0765886fb2421
Alexander Trieschmann <alexander.trieschmann@posteo.de>
2024-05-18 17:42:05 +0200

    fix deprecation warning Rails.application.secrets

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -16,6 +16,9 @@ Devise.setup do |config|
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '7c5367fe8ae33de86f30c2165725ba4af9847ab4a4e48fe305cf'
 
+  # TODO: Check this - this omits deprecation warning
+  config.secret_key = Rails.application.secret_key_base
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
